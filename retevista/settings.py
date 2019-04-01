553533a22ea7203973e8fabb68ea9cbd30292ad3
pierluigiderosa <pierluigi.derosa@gmail.com>
2019-04-01 16:01:01 +0200

    Ready to heroku this sucker in the face.

--- retevista/settings.py
+++ retevista/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 from decouple import config
 import socket
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -161,3 +162,6 @@ CRON_CLASSES = [
 DJANGO_CRON_LOCKFILE_PATH = [
 "/tmp",
 ]
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
